a3d66f81d352f9c7594a34e93b95b01fe6870378
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 00:03:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  if num < 0
+    new_num = num * -1
+  else
+    new_num = num
+  end
+  if new_num == 1 || new_num == 0
+    return false
+  end
+  (2...new_num).each do |factor|
+    if new_num % factor == 0
+      return false
+    end
+  end
+  return true
+end
